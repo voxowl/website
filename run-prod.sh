5ddb8f86f205b119c211cc249e42b5aa92cb03ef
Adrien Duermael <adrien@duermael.com>
2019-07-19 16:38:23 +0200

    run docker-compose in a container

--- run-prod.sh
+++ run-prod.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 docker network create websites 2> /dev/null
-docker-compose -f docker-compose.yml -f docker-compose-prod.yml up
+
+docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:$PWD" \
+    -w="$PWD" \
+    docker/compose:1.24.0 \
+    -f docker-compose.yml -f docker-compose-prod.yml \
+    up -d --build --remove-orphans
